0c0ae97d6df392aea34b5b3ceca8c15e589b4800
Odai Gubran <odaimansour777@gmail.com>
2025-06-10 21:07:32 +0300

    Added a “New Book” button that brings up a form allowing users to input the details for the new book

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ function Book(title, author, pages, readStatus) {
     this.id = crypto.randomUUID();
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(title, author, pages, readStatus) {
     // takes params, create a book then store it in the addBookToLibrary
-    const book1 = new Book("Rich dad poor dad", "James ahmed", 5)
+    const book1 = new Book(title, author, pages,  readStatus)
     myLibrary.push(book1)
     console.log(myLibrary)
 }
@@ -35,7 +35,42 @@ function displayBooks() {
         row.append(readStatusCol)
     })
 }
+
+
 document.addEventListener('DOMContentLoaded', () => {
-    addBookToLibrary();
     displayBooks();
+
+    const dialog = document.querySelector("dialog");
+    const newBookBtn = document.querySelector("#newBookBtn");
+    const newBookForm = document.querySelector("dialog form");
+
+
+    // New book button opens the dialog modally
+    newBookBtn.addEventListener("click", () => {
+        dialog.showModal();
+    })
+    
+    // Save button saves the input and close the dialog 
+    newBookForm.addEventListener("submit", event => {
+        event.preventDefault();
+
+        // Get input fom the form
+        const title = document.querySelector(".title-input").value.split();
+        const author = document.querySelector(".author-input").value.split();
+        const pages = document.querySelector(".pages-input").value.split();
+        let readStatus = false;
+        document.querySelectorAll("input[name='read_status']").forEach(radio => {
+            if (radio.checked) {
+                readStatus = radio.value;
+            }
+        })
+        // Save the book to the library
+        addBookToLibrary(title, author, pages, readStatus);
+
+        // close the dialog and displayBooks
+        dialog.close();
+        displayBooks();
+    })
+    
+    
 })
